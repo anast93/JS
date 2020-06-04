e60665646e9a69afa29ca33282167887ab8d8600
Anastasia Davydenko <latysheva93@list.ru>
2020-06-04 14:34:47 +0700

    Добавлен кусок с регулярными выражениями

--- script2.js
+++ script2.js
@@ -8,3 +8,9 @@ let mission = 1000000; // цель
 let period = 5; // период
 
 console.log( `Цель достигнута на ${((income-addExpenses)*period+deposit)/mission*100}%` );
+
+//Регулярные выражения
+var  reg1 = new RegExp('abc'); // определение шаблона с помощью создания объекта RegExp
+console.log(reg1.test('abcd')); // проверка. Результат - true.
+console.log(reg1.test('qwwertyabc')); // true
+console.log(reg1.test('qwwerty')); // false
