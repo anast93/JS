498a63c26c9cf6845a8a61d8442272cc257b9b80
Anastasia Davydenko <latysheva93@list.ru>
2020-06-04 13:50:11 +0700

    Присвоила значения переменным и добавила console.log

--- script2.js
+++ script2.js
@@ -1,10 +1,10 @@
 alert('Hello, world!'); // выводит модальное окно
 
 let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let income = 50000; // доход
+let addExpenses = 18000 ; // расход
+let deposit = 25000; // депозит в банке
+let mission = 1000000; // цель
+let period = 5; // период
 
-
+console.log( `Цель достигнута на ${((income-addExpenses)*period+deposit)/mission*100}%` );
