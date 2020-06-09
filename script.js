000fd7b12a3c50b8be2198d7055e5db197f25db7
Anastasia Davydenko <latysheva93@list.ru>
2020-06-09 14:14:11 +0700

    урок_4

--- script.js
+++ script.js
@@ -1,46 +1,70 @@
 'use sctrict'
 
-let money = prompt('Ваш месячный доход?'); // money присваивается значение, к-е вводит юзер
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = confirm('Есть ли у вас депозит в банке?'); 
+let money = +prompt('Ваш месячный доход?', 50000); // money присваивается значение, к-е вводит юзер
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Питание, проезд, ЖКХ');
+let deposit = confirm('Есть ли у вас депозит в банке?'); // Ok - true, ОТмена - false
 
-let expenses1 = prompt('Введите обязательную статью расходов.');
-let expenses2 = prompt('Введите обязательную статью расходов.');
+let expenses1 = prompt('Введите обязательную статью расходов.', 'Интернет');
+let expenses2 = prompt('Введите обязательную статью расходов.', 'Связь');
 
-let amount1 = prompt('Во сколько это обойдется?'); // Величины обяз. статей расходов
-let amount2 = prompt('Во сколько это обойдется?');
+let amount1 = +prompt('Во сколько это обойдется?', 1000); // Величины обяз. статей расходов
+let amount2 = +prompt('Во сколько это обойдется?', 500);
 
 let income = 'фриланс'; // Дополнительный доход
 let mission = 1000000; // цель
 let period = 12; 
 
-console.log( typeof(money) );
-console.log( typeof(income) );
-console.log( typeof(deposit) );
+// Ф-я выводит тип данных
+let showTypeOf = function(data) {
+    console.log( data, typeof (data) );
+}
 
-console.log( addExpenses.length );
+// Вызов ф-ии, к-ая выводит тип данных 
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
-console.log( `Период равен ${period} месяцев. Цель - заработать ${mission} рублей.` );
+// Считает сумму обязательных расходов за месяц
+let getExpensesMonth = function(a, b) {
+    return a + b;
+}
+console.log( 'Расходы за месяц: ' + getExpensesMonth(amount1, amount2) + ' рублей.' );
 
+// Приводит строку с расходами к нижнему регистру и разбивает на массив
 let str = addExpenses.toLowerCase();
 console.log( str.split(',') );
 
+// Считает накопления за месяц: доходы - обяз.расходы.
+let getAccumulatedMonth = function(a, b) {
+    return a - b;
+};
 
-let budgetMonth = +money-(+amount1 + +amount2); // Бюджет на месяц с учетом обяз. расходов
-let budgetDay = Math.floor(budgetMonth/30); // Бюджет на день
+const accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth(amount1, amount2)); 
+// накопления за месяц, переменной присвоено значение ф-ии, которая их считает
 
-console.log( `Бюджет на месяц: ${budgetMonth} рублей.` );
+let budgetDay = Math.floor(accumulatedMonth/30); // Бюджет на день. Округляет значение в меньшую сторону.
 console.log( `Бюджет на день: ${budgetDay} рублей.` );
-console.log( `Цель будет достигнута за ${Math.ceil(mission/budgetMonth)} месяцев.` ); // округляет кол-во
+
+//console.log( `Цель будет достигнута за ${Math.ceil(mission/budgetMonth)} месяцев.` ); // округляет кол-во
 // месяцев в большую сторону
 
-if (budgetDay > 1200) {
-    console.log( 'У вас высокий уровень дохода!' );
-} else if (budgetDay > 600) {
-    console.log( 'У вас средний уровень дохода.');
-} else if (budgetDay >= 0) {
-    console.log('К сожалению, у вас уровень дохода ниже среднего.');
-} else {
-    console.log( 'Что-то пошло не так.' );
-}
+// Считает период, за который будет достигнута цель
+let getTargetMonth = function(a, b) {
+    return Math.ceil( a/b );
+};
 
+console.log( 'Цель будет достигнута за ' + getTargetMonth(mission, accumulatedMonth) + ' месяцев.' );
+
+let geetStatusIncome = function() {
+    if (budgetDay > 1200) {
+        return ( 'У вас высокий уровень дохода!' );
+    } else if (budgetDay > 600) {
+        return ( 'У вас средний уровень дохода.');
+    } else if (budgetDay >= 0) {
+        return ('К сожалению, у вас уровень дохода ниже среднего.');
+    } else {
+        return ( 'Что-то пошло не так.' );
+    }
+};
+
+console.log( geetStatusIncome() );
