ba5bbc35af11c651b6fe85b7eb215ba0bed68e9f
Anastasia Davydenko <latysheva93@list.ru>
2020-06-10 23:23:21 +0700

    урок_5

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
-'use sctrict'
+'use strict'
 
-let money = +prompt('Ваш месячный доход?', 50000); // money присваивается значение, к-е вводит юзер
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Питание, проезд, ЖКХ');
-let deposit = confirm('Есть ли у вас депозит в банке?'); // Ok - true, ОТмена - false
+// Ф-я выполняет проверку на число. isNan проверяет явл-ся ли параметр нечисловым значением(NaN) или нет.
+// Функция isFinite преобразует параметр к числу и возвращает true, если это НЕ Infinity, 
+//-Infinity или NaN.
+// Ф-я возвращает true, когда параметр - число.
+let isNumber = function(testNumber) {
+    return !isNaN( parseFloat(testNumber) ) && isFinite(testNumber);
+};
 
-let expenses1 = prompt('Введите обязательную статью расходов.', 'Интернет');
-let expenses2 = prompt('Введите обязательную статью расходов.', 'Связь');
+let money; // доход
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Питание,проезд,ЖКХ');
+let deposit = confirm('Есть ли у вас депозит в банке?'); // Ok - true, Отмена - false
 
-let amount1 = +prompt('Во сколько это обойдется?', 1000); // Величины обяз. статей расходов
-let amount2 = +prompt('Во сколько это обойдется?', 500);
+let expenses = []; // массив с обяз.доходами
+
+// Ф-я выводит окно с вопросом до тех пор, пока введенное значение - не число
+let start = function() {
+
+    do {
+        money = prompt('Ваш месячный доход?');
+    }
+    while( !isNumber(money) );
+
+};
+
+start();
+
+// Ф-я сохраняет обязательные расходы в массив и считает сумму расходов
+let getExpensesMonth = function() {
+    let sum = 0;
+    let amount;
+    let arrAmount = [];
+
+    for (let i = 0; i < 2; i++) {
+            expenses[i] = prompt('Введите обязательную статью расходов.', 'Интернет');  
+            amount = prompt('Во сколько это обойдется?');
+
+        while(!isNumber(amount)) {
+            amount = prompt('Во сколько это обойдется?');
+        }
+        arrAmount.push(+amount); // метод push добавляет элемент в конец массива
+        sum = arrAmount.reduce((prev, cur) => prev + cur); // складывает эл-ты массива
+       
+    }
+    return sum;
+};
+
+let expensesAmount = getExpensesMonth();
 
 let income = 'фриланс'; // Дополнительный доход
 let mission = 1000000; // цель
@@ -17,54 +55,54 @@ let period = 12;
 // Ф-я выводит тип данных
 let showTypeOf = function(data) {
     console.log( data, typeof (data) );
-}
+};
 
 // Вызов ф-ии, к-ая выводит тип данных 
 showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
-// Считает сумму обязательных расходов за месяц
-let getExpensesMonth = function(a, b) {
-    return a + b;
-}
-console.log( 'Расходы за месяц: ' + getExpensesMonth(amount1, amount2) + ' рублей.' );
+console.log( 'Расходы за месяц: ' + expensesAmount + ' рублей.' );
 
 // Приводит строку с расходами к нижнему регистру и разбивает на массив
 let str = addExpenses.toLowerCase();
 console.log( str.split(',') );
 
 // Считает накопления за месяц: доходы - обяз.расходы.
-let getAccumulatedMonth = function(a, b) {
-    return a - b;
+let getAccumulatedMonth = function(getMoney, getExpensesAmount) {
+    return getMoney - getExpensesAmount;
 };
 
-const accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth(amount1, amount2)); 
-// накопления за месяц, переменной присвоено значение ф-ии, которая их считает
+const accumulatedMonth = getAccumulatedMonth(money, expensesAmount); 
+// накопления за месяц; переменной присвоено значение ф-ии, которая их считает
 
 let budgetDay = Math.floor(accumulatedMonth/30); // Бюджет на день. Округляет значение в меньшую сторону.
 console.log( `Бюджет на день: ${budgetDay} рублей.` );
 
-//console.log( `Цель будет достигнута за ${Math.ceil(mission/budgetMonth)} месяцев.` ); // округляет кол-во
-// месяцев в большую сторону
 
 // Считает период, за который будет достигнута цель
-let getTargetMonth = function(a, b) {
-    return Math.ceil( a/b );
-};
-
-console.log( 'Цель будет достигнута за ' + getTargetMonth(mission, accumulatedMonth) + ' месяцев.' );
-
-let geetStatusIncome = function() {
-    if (budgetDay > 1200) {
-        return ( 'У вас высокий уровень дохода!' );
-    } else if (budgetDay > 600) {
-        return ( 'У вас средний уровень дохода.');
-    } else if (budgetDay >= 0) {
-        return ('К сожалению, у вас уровень дохода ниже среднего.');
+let getTargetMonth = function(getMission, getAccumulation) {
+    let time = Math.ceil(getMission/getAccumulation);
+    
+    if (time <= 0) {
+        return console.log( 'Цель не будет достигнута.' ) ;
     } else {
-        return ( 'Что-то пошло не так.' );
+        return console.log( 'Цель будет достигнута за ' + time + ' месяцев.' );
     }
 };
 
-console.log( geetStatusIncome() );
+getTargetMonth(mission, accumulatedMonth);
+
+let geetStatusIncome = function(getBudgetDay) {
+    if (getBudgetDay > 1200) {
+        return ( 'У вас высокий уровень дохода!' );
+    } else if (getBudgetDay > 600) {
+        return ( 'У вас средний уровень дохода.');
+    } else if (getBudgetDay >= 0) {
+        return ('К сожалению, у вас уровень дохода ниже среднего.');
+    } else {
+        return ( 'Ваши расходы превышают доходы.' );
+    }
+};
+
+console.log( geetStatusIncome(budgetDay) );
